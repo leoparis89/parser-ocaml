da77f029055f52680a444648db2931929539c3bb
Lev Kowalski <lev.kowalski@ahrefs.com>
2025-03-04 17:40:51 +0100

    wip

--- lib/Tokenizer.ml
+++ lib/Tokenizer.ml
@@ -18,12 +18,12 @@ let match_and_capture pattern str =
 (* Example usage *)
 
 type token = Semi_colon | String
-type matcher = { pattern : Re.re; token : token }
+type matcher = { pattern : Re.re; token : token option }
 
 let matchers =
   [
-    { pattern = re_pattern "^([a-z]+)$"; token = String };
-    { pattern = re_pattern "^;$"; token = Semi_colon };
+    { pattern = re_pattern "^([a-z]+)$"; token = Some String };
+    { pattern = re_pattern "^;$"; token = Some Semi_colon };
   ]
 
 type t = { _string : string; cursor : int }
@@ -31,8 +31,17 @@ type t = { _string : string; cursor : int }
 let is_EOF t = t.cursor >= String.length t._string
 let has_more_tokens t = t.cursor < String.length t._string
 
-let foo pattern str (t : t) =
+let match_token pattern str (t : t) =
   match match_and_capture pattern str with
   | Some result ->
       (Some result, { t with cursor = t.cursor + String.length result })
   | None -> (None, t)
+
+let rec get_next_token t (matchers : matcher list) =
+  match matchers with
+  | [] -> (None, t)
+  | matcher :: matchers -> (
+      let token_value, t = match_token matcher.pattern t._string t in
+      match token_value with
+      | Some result -> (Some (matcher.token, result), t)
+      | None -> get_next_token t matchers)
