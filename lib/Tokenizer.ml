6277d482c577e451fa5d95cdf4367117d096bf49
Lev Kowalski <lev.kowalski@ahrefs.com>
2025-03-04 15:36:21 +0100

    wip

--- lib/Tokenizer.ml
+++ lib/Tokenizer.ml
@@ -1,6 +1,3 @@
-type token = Semi_colon | String
-type t = { _string : string; cursor : int }
-
 let re_pattern str = Re.compile (Re.Perl.re str)
 
 let match_regex pattern str =
@@ -8,9 +5,6 @@ let match_regex pattern str =
 
 let get_capture group match_result =
   try Some (Re.Group.get match_result group) with Not_found -> None
-
-let is_EOF t = t.cursor >= String.length t._string
-let has_more_tokens t = t.cursor < String.length t._string
 
 (* Example usage *)
 let pattern = re_pattern "hello ([a-z]+)"
@@ -24,3 +18,9 @@ let _ =
       | Some captured -> Printf.printf "Captured: %s\n" captured
       | None -> Printf.printf "No capture found\n")
   | None -> Printf.printf "No match\n"
+
+type token = Semi_colon | String
+type t = { _string : string; cursor : int }
+
+let is_EOF t = t.cursor >= String.length t._string
+let has_more_tokens t = t.cursor < String.length t._string
