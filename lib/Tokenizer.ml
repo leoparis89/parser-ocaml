191c5c1b46e94bf56a03be3802c44138fccec1b6
Lev Kowalski <lev.kowalski@ahrefs.com>
2025-03-04 15:27:45 +0100

    wip

---
+++ lib/Tokenizer.ml
@@ -0,0 +1,5 @@
+type token = Semi_colon | String
+type t = { _string : string; cursor : int }
+
+let is_EOF t = t.cursor >= String.length t._string
+let has_more_tokens t = t.cursor < String.length t._string
