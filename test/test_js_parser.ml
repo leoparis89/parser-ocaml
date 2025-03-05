c1d4549b15ec67c57378c5c890390911e5977841
Lev Kowalski <lev.kowalski@ahrefs.com>
2025-03-05 16:34:24 +0100

    wip

--- test/test_js_parser.ml
+++ test/test_js_parser.ml
@@ -1,13 +1,5 @@
 (* Build with `ocamlbuild -pkg alcotest simple.byte` *)
 
-(* A module with functions to test *)
-module To_test = struct
-  (* let lowercase = String.lowercase_ascii *)
-  let capitalize = String.capitalize_ascii
-  let str_concat = String.concat ""
-  let list_concat = List.append
-end
-
 open Js_parser
 open Tokenizer
 
@@ -21,18 +13,7 @@ let test_regex () =
     "regex simple test" (Some "world")
     (Regex.match_and_capture pattern str)
 
-let test_capitalize () =
-  Alcotest.(check string) "same string" "World." (To_test.capitalize "world.")
-
-let test_str_concat () =
-  Alcotest.(check string)
-    "same string" "foobar"
-    (To_test.str_concat [ "foo"; "bar" ])
-
-let test_list_concat () =
-  Alcotest.(check (list int))
-    "same lists" [ 1; 2; 3 ]
-    (To_test.list_concat [ 1 ] [ 2; 3 ])
+let test_parser () = assert true
 
 (* Run it *)
 let () =
@@ -42,8 +23,6 @@ let () =
       ( "string-case",
         [
           test_case "Simple regex test" `Quick test_regex;
-          test_case "Capitalization" `Quick test_capitalize;
+          test_case "Test parser" `Quick test_parser;
         ] );
-      ("string-concat", [ test_case "String mashing" `Quick test_str_concat ]);
-      ("list-concat", [ test_case "List mashing" `Slow test_list_concat ]);
     ]
